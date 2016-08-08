2e96c9e63fb75b7f465963d436225751937ef662
Michael Tobin <mikel.tobin@gmail.com>
2016-08-07 21:56:21 -0600

    Fun page fixed, routes set up

--- routes/fun.js
+++ routes/fun.js
@@ -5,7 +5,7 @@ var request = require('request');
 
 
 
-  router.get('/getFun', function(req, res, next) {
+  router.get('/getQuote', function(req, res, next) {
     request("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en", function (error, response, body) {
       if (!error && response.statusCode == 200) {
        res.send(body);
@@ -13,14 +13,22 @@ var request = require('request');
     })
   });
 
-  router.post('/getFunny', function(req, res, next) {
-
-    request(`http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en`, function (error, response, body) {
-
-      if (!error && response.statusCode == 200) {
-        res.send(body);
-      }
+  router.get('/getWord', function(req,res,next){
+    request("http://api.wordnik.com:80/v4/words.json/wordOfTheDay?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5", function(error,response,body){
+      res.send(JSON.parse(body));
     })
-  });
+  })
+
+  router.get('/getAdvice', function(req,res,next){
+    request("http://api.adviceslip.com/advice", function(error,response,body){
+      res.send(JSON.parse(body));
+    })
+  })
+
+  router.get('/getChuck', function(req,res,next){
+    request("https://api.chucknorris.io/jokes/random", function(error,response,body){
+      res.send(JSON.parse(body));
+    })
+  })
 
   module.exports = router;
