acf746ce751105222553dea1b09fde5c39e660f3
Michael Tobin <mikel.tobin@gmail.com>
2016-08-07 18:40:26 -0600

    Changed weather route'

--- routes/weather.js
+++ routes/weather.js
@@ -5,23 +5,30 @@ var request = require('request');
 
 
 
-  router.post('/getWeather', function(req, res, next) {
-    var weatherAPIKey = 'c98ec93f5a134adb4a37ca10c015d4e5';
-    var long = req.body.lng
-    var lat = req.body.lat
-    request(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=c98ec93f5a134adb4a37ca10c015d4e5`, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-       var obj = JSON.parse(body);
-       var arr = obj;
-       var cityName = obj.name;
-       var degrees = Math.ceil(arr.main.temp)+'°';
-       var weather = arr.weather[0].description;
-       var dateAndWeather = [];
-       dateAndWeather.push(cityName+": "+ weather + " " + degrees);
-      //  console.log(dateAndWeather);
-       res.send(body);
-      }
-    })
-  });
+router.post('/getWeather', function(req, res, next) {
+	var weatherAPIKey = 'c98ec93f5a134adb4a37ca10c015d4e5';
+	var long = req.body.lng
+	var lat = req.body.lat
+	request(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=c98ec93f5a134adb4a37ca10c015d4e5`, function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var obj = JSON.parse(body);
+			var arr = obj;
+			var cityName = obj.name;
+			var degrees = Math.ceil(arr.main.temp) + '°';
+			var weather = arr.weather[0].description;
+			var dateAndWeather = [];
+			dateAndWeather.push(cityName + ": " + weather + " " + degrees);
+			//  console.log(dateAndWeather);
+			res.send(body);
+		}
+	})
+});
 
-  module.exports = router;
+router.post('/getWeekWeather', function(req, res, next) {
+	request(`http://api.openweathermap.org/data/2.5/forecast/daily?lat=${req.body.lat}&lon=${req.body.lng}&units=imperial&cnt=5&APPID=c98ec93f5a134adb4a37ca10c015d4e5`, function(error, response, body) {
+		var parsedBody = JSON.parse(body);
+		res.send(parsedBody);
+	})
+});
+
+module.exports = router;
